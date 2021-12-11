fe100fc585e86f43deb11c5587eba872718baf53
PedroViniciusConrado <pvc.programacao@gmail.com>
2021-12-11 00:45:07 -0300

    Terceiro commit

--- LaçosForeWhile.js
+++ LaçosForeWhile.js
@@ -1,2 +1,30 @@
-let numeroDeAlunos = 10;
-console.log (numeroDeAlunos);
+const entrada = require ('prompt-sync')({sigint: true});
+
+let numeroDeAlunos = entrada ("Qual é o número de alunos? ");
+
+let contador = 0;
+
+console.log (` \n\#######################\n\       Laço WHILE\n\#######################\n\ `);
+
+while (contador <= numeroDeAlunos) {
+    if (contador == 0)    {
+        console.log ("Este número é " + contador + ".");
+    } else if (contador % 2 == 0) {
+        console.log (`O número ${contador} é par!`);
+    } else {
+        console.log (`O número ${contador} é ímpar!`);
+    }
+    contador++;
+}
+
+console.log (` \n\#######################\n\       Laço FOR\n\#######################\n\ `);
+
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    if (contador == 0)    {
+        console.log ("Este número é " + contador + ".");
+    } else if (contador % 2 == 0) {
+        console.log (`O número ${contador} é par!`);
+    } else {
+        console.log (`O número ${contador} é ímpar!`);
+    }
+}
